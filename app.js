6fa7db6a2248623eb26c91b14be4dc35ee218019
b13nmaN <blenmanjoel@gmail.com>
2023-11-14 09:23:05 -0400

    added console log statement

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.getElementById('searchButton').addEventListener('click', function() {
   fetch('superheroes.php')
     .then(response => response.text())  // Change response type to text
+    console.log(response.text())
     .then(data => {alert(data)
     });
 });
